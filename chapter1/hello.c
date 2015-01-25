4d99e0773e4b64b019270d7e89a8b7a635c82d72
xOpenLee <750haige@gmail.com>
2015-01-25 15:29:37 +0800

    Getting Started

---
+++ chapter1/hello.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.c
+	> Author: haibin
+	> Mail: haibin2011@163.com 
+	> Created Time: Sunday, January 25, 2015 PM03:27:03 HKT
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main()
+{
+    printf("\r\n###INFO:Hello the C programming language\r\n");
+    return 0;
+}
